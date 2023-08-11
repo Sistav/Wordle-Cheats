08d2dadb9c0a46b398447e9ad70352a5c7d07433
Georgios Dialynas-Vatsis <47365973+Sistav@users.noreply.github.com>
2023-08-11 16:32:59 -0400

    added cli interface

--- Letters.py
+++ Letters.py
@@ -1,18 +1,30 @@
 # Create Letter Class
 class Letter:
     letters = []
-    def __init__(self,character: str,in_word: bool,not_position=[]) -> None:
+    def __init__(self,character: str,in_word: bool,not_position=[],guaruntee_position=[]) -> None:
         self.character = character.lower()
         self.in_word = in_word
         self.not_position = not_position
+        self.guaruntee_position = guaruntee_position
         Letter.letters.append(self)
+
+    def check_existing_letters(letter):
+        for i in range(len(Letter.letters)):
+            if Letter.letters[i].character == letter.lower():
+                return Letter.letters[i]
+        return None
+
     # Check a word against a specific letter
     def check_letter(self,word):
+        # Check if the guaranteed letters are in the word
+        for i in range(len(self.guaruntee_position)):
+            if word[self.guaruntee_position[i]] != self.character:
+                return False
         # If the letter isn't in the word
         if self.in_word == False and (self.character in word):
             return False
         # Position Stuff
-        elif self.in_word and (self.character in word) and len(self.not_position) >=1 and ((word.index(self.character)+1) in self.not_position):
+        elif self.in_word and (self.character in word) and len(self.not_position) >= 1 and ((word.index(self.character)) in self.not_position):
             return False
         # If the letter is supposed to be in the word but isn't
         elif self.in_word and not (self.character in word) and len(self.not_position):
--- Main.py
+++ Main.py
@@ -3,21 +3,65 @@ from words import words
 from Letters import Letter
 
 def get_current_word():
-    pass
+    while True:
+        currentword = input("Please enter the word used:\n")
+        if currentword.isalpha() and len(currentword) == 5:
+            return currentword
+        else:
+            print("That is an invalid word")
 
-def get_letter_status():
-    pass
+def extract_letters(word):
+    for i in range(len(word)):
+
+        existing = Letter.check_existing_letters(word[i])
+
+        if existing == None:
+            current_status = get_letter_status(word[i])
+            if current_status == 1:
+                Letter(word[i],True,guaruntee_position = [i])
+            elif current_status == 2:
+                Letter(word[i],True,[i])
+            else:
+                Letter(word[i],False)
+        elif (existing.in_word != False):
+            current_status = get_letter_status(word[i],True)
+            if current_status == 1:
+                existing.guaruntee_position.append(i)
+            if current_status == 2:
+                existing.not_position.append(i)
+            
+        
+def get_letter_status(letter,in_word=False):
+    while True:
+        if in_word == False:
+            letter_status = input(f"What color was the letter '{letter}'?:\n1)Green\n2)Yellow\n3)Gray\n")
+            if letter_status.isnumeric() and int(letter_status) >= 1 and int(letter_status) <= 3:
+                    return int(letter_status)
+            else:
+                print("That is an invalid input")
+        else:
+            letter_status = input(f"What color was the letter '{letter}'?:\n1)Green\n2)Yellow")
+            if letter_status.isnumeric() and(int(letter_status) == 1 or int(letter_status) == 2) :
+                    return int(letter_status)
+            else:
+                print("That is an invalid input")
+
 
 def main():
-    possible_words = []
-    for i in range(len(words)):
-        if Letter.check(words[i]):
-            possible_words.append(words[i])
+    for i in range(5):
+        word = get_current_word()
+        extract_letters(word)
+        possible_words = []
+        for i in range(len(words)):
+            if Letter.check(words[i]):
+                possible_words.append(words[i])
 
-    for i in possible_words:
-        print(i)
-    while True:
-        current_word
+        for i in possible_words:
+            print(i)
+    
+    
+
+        
 
 if __name__ == "__main__":
     main()
